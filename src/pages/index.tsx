0b2d45de20d5480b5cf7e1ef2788515036c8cedc
em0-omg <em0818.omg@gmail.com>
2022-01-02 19:37:22 +0900

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <Seo title="Home" />
+    <h1>Hello World!</h1>
+  </Layout>
+)
+
+export default IndexPage
